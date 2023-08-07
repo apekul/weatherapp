f87c02b226f3bb65c1828eff498041056fce5fc8
Adaś <adampekul@gmail.com>
2023-08-08 00:05:32 +0200

    comment userLocation

--- src/App.js
+++ src/App.js
@@ -9,30 +9,30 @@ function App() {
   const [city, setCity] = useState();
   // const [userLocation, setUserLocation] = useState();
 
-  let options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
+  // let options = {
+  //   enableHighAccuracy: true,
+  //   timeout: 5000,
+  //   maximumAge: 0,
+  // };
 
-  function success(pos) {
-    // var crd = pos.coords;
-    // setUserLocation({ lat: crd.latitude, lon: crd.longitude });
-  }
+  // function success(pos) {
+  //   var crd = pos.coords;
+  //   setUserLocation({ lat: crd.latitude, lon: crd.longitude });
+  // }
 
-  function errors(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
+  // function errors(err) {
+  //   console.warn(`ERROR(${err.code}): ${err.message}`);
+  // }
 
   const fetchData = (lat, lon) => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     )
       .then((res) => res.json())
       .then((res) => setData(res))
       .catch((error) => console.log(error));
     fetch(
-      `http://api.openweathermap.org/data/2.5/air_pollution?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${API_KEY}`
+      `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`
     )
       .then((res) => res.json())
       .then((res) => setPollution(res))
@@ -40,26 +40,25 @@ function App() {
   };
 
   // User geolocation
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.permissions
-        .query({ name: "geolocation" })
-        .then(function (result) {
-          if (result.state === "granted") {
-            navigator.geolocation.getCurrentPosition(success, errors, options);
-            //If granted then you can directly call your function here
-          } else if (result.state === "prompt") {
-            navigator.geolocation.getCurrentPosition(success, errors, options);
-            //If prompt then the user will be asked to give permission
-          } else if (result.state === "denied") {
-            //If denied then you have to show instructions to enable location
-          }
-        });
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   if (navigator.geolocation) {
+  //     navigator.permissions
+  //       .query({ name: "geolocation" })
+  //       .then(function (result) {
+  //         if (result.state === "granted") {
+  //           navigator.geolocation.getCurrentPosition(success, errors, options);
+  //           //If granted then you can directly call your function here
+  //         } else if (result.state === "prompt") {
+  //           navigator.geolocation.getCurrentPosition(success, errors, options);
+  //           //If prompt then the user will be asked to give permission
+  //         } else if (result.state === "denied") {
+  //           //If denied then you have to show instructions to enable location
+  //         }
+  //       });
+  //   } else {
+  //     console.log("Geolocation is not supported by this browser.");
+  //   }
+  // }, []);
 
   useEffect(() => {
     // let storage = JSON.parse(localStorage.getItem("weather"))[0].city.coord;
